3c8315b118554af239fd32f3924ed4b527daa56d
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-09-16 15:36:50 +0400

    recuperation de donne

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+/*
 let mesPlaces = document.querySelectorAll('.zone01 input');
 
 console.log(mesPlaces);
 
 
-
 function checkboxCocher(){
-    if(document.querySelector('#01').checked == true){
+    if(document.getElementById('#01').checked == true){
         console.log(mesPlaces.indexOf, "celui-ci est coché");
     }
     else{
@@ -15,3 +15,35 @@ function checkboxCocher(){
 checkboxCocher();
 
 console.log(mesPlaces.checked);
+*/
+
+let range = document.querySelector('#range');
+let colonne = document.querySelector('#colonne');
+let bouton = document.querySelector(".bouton");
+//let monTableau = [];
+let rangerDemander;
+let colonneDemander;
+
+
+//console.log(range.value);
+//console.log(colonne);
+
+function generer(){
+    bouton.onclick = function(){
+        //console.log(range.value, colonne.value);
+        //console.log(range.value);
+       // console.log(colonne.value);
+        rangerDemander = range.value;
+        colonneDemander = colonne.value;
+        //monTableau.push(range.value, colonne.value);
+    }
+}
+
+
+console.log(rangerDemander);
+console.log(colonneDemander);
+//console.log(monTableau);
+
+
+generer();
+
